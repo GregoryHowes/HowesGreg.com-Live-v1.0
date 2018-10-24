1dda0c7ecf141160122100319ef02514756a97a5
GregoryHowes <33244619+GregoryHowes@users.noreply.github.com>
2018-10-24 18:45:01 +0900

    Added Japanese translations

--- js/setlanguage.js
+++ js/setlanguage.js
@@ -1,15 +1,39 @@
 function setLanguage() {
     //window.alert("Change Language");
-    if (document.getElementById("langToggle").value == "jp" )
+    //if (document.getElementById("langToggle").value == "jp" )
+    if (document.getElementById("langToggle").innerHTML == "日本語" )
     {
+        //set language toggle caption to English
+        document.getElementById("langToggle").innerHTML = "English";
+
         document.getElementById("myName").innerHTML = "ハウズ　グレッグ";
         document.getElementById("myNameNavbar").innerHTML = "ハウズ　グレッグ";
-        document.getElementById("aboutMeNav").innerHTML =　"グレッグは？";
+        document.getElementById("aboutMeNav").innerHTML =　"プロフィール";
         document.getElementById("trainingNav").innerHTML =　"トレーニング";
         document.getElementById("galleryNav").innerHTML =　"写真";
         document.getElementById("blogNav").innerHTML =　"ブログ";
         document.getElementById("contactNav").innerHTML =　"お問い合わせ";
+        document.getElementById("aboutMeCaption").innerHTML =　"プロフィール";
+        document.getElementById("myBio").innerHTML =　"コナ出場を夢見ている";
+
+        document.getElementById("PBs").innerHTML = "記録";
+
+        document.getElementById("marathonPB").innerHTML =　"フルマラソン - 2:42:31";
+        document.getElementById("marathonPBRace").innerHTML =　"熊本城マラソン 2015";
+        
+        document.getElementById("halfPB").innerHTML =　"ハーフマラソン - 1:14:49";
+        document.getElementById("halfPBRace").innerHTML =　"天草マラソン 2014";
+
+        document.getElementById("10kPB").innerHTML =　"10キロ - 33:41";
+        document.getElementById("10kPBRace").innerHTML =　"玉名いちごマラソン　2013";
+        
+        document.getElementById("IMPB").innerHTML =　"ウルトラロングトライアスロン - 9:49:40";
+        document.getElementById("IMPBRace").innerHTML =　"五島列島国際トライアスロン 2018";
+        
     } else {
+        //set language toggle caption to Japanese
+        document.getElementById("langToggle").innerHTML = "日本語";
+
         document.getElementById("myName").innerHTML = "Greg Howes";
         document.getElementById("myNameNavbar").innerHTML = "Greg Howes";
         document.getElementById("aboutMeNav").innerHTML =　"About me";
@@ -17,5 +41,22 @@ function setLanguage() {
         document.getElementById("galleryNav").innerHTML =　"Gallery";
         document.getElementById("blogNav").innerHTML =　"Blog";
         document.getElementById("contactNav").innerHTML =　"Contact";
+        document.getElementById("aboutMeCaption").innerHTML =　"About Me";
+        document.getElementById("myBio").innerHTML =　"From multisport muppet to dreams of Kona";
+
+        document.getElementById("PBs").innerHTML = "Personal Bests";
+
+        document.getElementById("marathonPB").innerHTML =　"Marathon - 2:42:31";
+        document.getElementById("marathonPBRace").innerHTML =　"Kumamoto 2015";
+
+        document.getElementById("halfPB").innerHTML =　"Half Marathon 1:14:49";
+        document.getElementById("halfPBRace").innerHTML =　"Amakusa 2014";
+
+        document.getElementById("10kPB").innerHTML =　"10K - 33:41";
+        document.getElementById("10kPBRace").innerHTML =　"Tamana Ichigo 2013";
+
+        document.getElementById("IMPB").innerHTML =　"IM-distance triathlon - 9:49:40";
+        document.getElementById("IMPBRace").innerHTML =　"Goto Nagasaki 2018";
+
     }
 }
